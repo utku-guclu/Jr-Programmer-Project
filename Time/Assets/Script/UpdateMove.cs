56ecfbf6725d4dcef1c886104489cb2c50e92f68
utku-guclu <utku_guclu@hotmail.com>
2023-12-04 02:52:37 +0300

    physics

---
+++ Time/Assets/Script/UpdateMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMove : MonoBehaviour
+{
+    float speed = 2f;
+    void Update()
+    {
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
---
+++ Time/Assets/Scripts/Shell.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour {
+
+    public GameObject explosion;
+    float speed = 0.0f;
+    float ySpeed = 0.0f;
+    float mass = 10.0f;
+    float force = 10.0f;
+    float drag = 1.0f;
+    float gravity = -9.8f;
+    float gAccel;
+    float acceleration;
+
+
+    void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.tag == "tank") {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start() {
+        // trigger
+        acceleration = force / mass;
+        speed += acceleration * 1;
+        gAccel = gravity / mass;
+    }
+
+    void LateUpdate() {
+        // physics
+        speed *= (1 - Time.deltaTime * drag);
+        ySpeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0, ySpeed, speed);
+    }
+}
